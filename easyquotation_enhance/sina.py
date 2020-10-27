e3d4cd7e585ee4b9b1928452e5402459a9198085
yhf2lj <yhf2lj@gmail.com>
2020-10-27 20:02:22 +0800

    1. sina数据时间错误可能错误修复

--- easyquotation_enhance/sina.py
+++ easyquotation_enhance/sina.py
@@ -85,40 +85,44 @@ class SinaQuotation(BaseDownload):
         stock_list = list()
         for stock_match_object in result:
             stock = stock_match_object.groups()
-            if len(stock[32].strip()) <= 6:
+            try:
+                stock_list.append(dict(
+                    code=stock[0],
+                    name=stock[1],
+                    open=float(stock[2]),
+                    close=float(stock[3]),
+                    now=float(stock[4]),
+                    high=float(stock[5]),
+                    low=float(stock[6]),
+                    buy=float(stock[7]),
+                    sell=float(stock[8]),
+                    turnover=int(stock[9]),
+                    volume=float(stock[10]),
+                    bid1_volume=int(stock[11]),
+                    bid1=float(stock[12]),
+                    bid2_volume=int(stock[13]),
+                    bid2=float(stock[14]),
+                    bid3_volume=int(stock[15]),
+                    bid3=float(stock[16]),
+                    bid4_volume=int(stock[17]),
+                    bid4=float(stock[18]),
+                    bid5_volume=int(stock[19]),
+                    bid5=float(stock[20]),
+                    ask1_volume=int(stock[21]),
+                    ask1=float(stock[22]),
+                    ask2_volume=int(stock[23]),
+                    ask2=float(stock[24]),
+                    ask3_volume=int(stock[25]),
+                    ask3=float(stock[26]),
+                    ask4_volume=int(stock[27]),
+                    ask4=float(stock[28]),
+                    ask5_volume=int(stock[29]),
+                    ask5=float(stock[30]),
+                    datetime=datetime.strptime(stock[31] + " " + stock[32], "%Y-%m-%d %H:%M:%S"),
+                ))
+            except ValueError:
                 continue
-            stock_list.append(dict(
-                code=stock[0],
-                name=stock[1],
-                open=float(stock[2]),
-                close=float(stock[3]),
-                now=float(stock[4]),
-                high=float(stock[5]),
-                low=float(stock[6]),
-                buy=float(stock[7]),
-                sell=float(stock[8]),
-                turnover=int(stock[9]),
-                volume=float(stock[10]),
-                bid1_volume=int(stock[11]),
-                bid1=float(stock[12]),
-                bid2_volume=int(stock[13]),
-                bid2=float(stock[14]),
-                bid3_volume=int(stock[15]),
-                bid3=float(stock[16]),
-                bid4_volume=int(stock[17]),
-                bid4=float(stock[18]),
-                bid5_volume=int(stock[19]),
-                bid5=float(stock[20]),
-                ask1_volume=int(stock[21]),
-                ask1=float(stock[22]),
-                ask2_volume=int(stock[23]),
-                ask2=float(stock[24]),
-                ask3_volume=int(stock[25]),
-                ask3=float(stock[26]),
-                ask4_volume=int(stock[27]),
-                ask4=float(stock[28]),
-                ask5_volume=int(stock[29]),
-                ask5=float(stock[30]),
-                datetime=datetime.strptime(stock[31] + " " + stock[32], "%Y-%m-%d %H:%M:%S"),
-            ))
+            except Exception as error:
+                print("错误数据：", stock)
+                print(error)
         return stock_list
